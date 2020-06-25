68c0afa1cf65aeeb330c158f7d2c9b6e17096047
Shailza Jolly <shailzajolly@gmail.com>
2020-06-25 20:05:00 +0200

    extract_topNP_accuracy added

--- extract_NPclass_acc.py
+++ extract_NPclass_acc.py
@@ -12,39 +12,39 @@ accuracy of these ids from tr_nonyn_yn_model_preds.pkl.
 top_NP_answers = json.load(open('data/top_np_ans.json','r')) #List of answers
 
 top_NP_answers_first352 = top_NP_answers
-#top_NP_answers_first352 = top_NP_answers[: 352]
-# top_NP_answers_last = top_NP_answers[352 :]
+top_NP_answers_first352 = top_NP_answers[: 352]
+top_NP_answers_last = top_NP_answers[352 :]
 
 validation_file = json.load(open('data/val_ann_np_ans.json','r'))['annotations'] #133540
 preds = pickle.load(open('tr_nonyn_yn_model_preds.pkl','rb')) #list of tuples
 
 topNP_ans_qids352 = []
-#topNP_ans_qidslast = []
+topNP_ans_qidslast = []
 
 for annos in validation_file:
 
 	if annos['multiple_choice_answer'] in top_NP_answers_first352:
 		topNP_ans_qids352.append(annos['question_id'])
-	# if annos['multiple_choice_answer'] in top_NP_answers_last:
-	# 	topNP_ans_qidslast.append(annos['question_id'])
+	if annos['multiple_choice_answer'] in top_NP_answers_last:
+		topNP_ans_qidslast.append(annos['question_id'])
 
 qid2score = {}
 for tup in preds:
 	qid2score[tup[0]] = tup[1]
 
 topNP_ans_score352 = []
-#topNP_ans_scorelast = []
+topNP_ans_scorelast = []
 
 for qid in topNP_ans_qids352:
 	if qid in qid2score:
 		topNP_ans_score352.append(qid2score[qid])
 
-# for qid in topNP_ans_qidslast:
-# 	if qid in qid2score:
-# 		topNP_ans_scorelast.append(qid2score[qid])
+for qid in topNP_ans_qidslast:
+	if qid in qid2score:
+		topNP_ans_scorelast.append(qid2score[qid])
 
 print("Score for top NP classes 352: ", np.mean(topNP_ans_score352))
-#print("Score for top NP classes last: ", np.mean(topNP_ans_scorelast))
+print("Score for top NP classes last: ", np.mean(topNP_ans_scorelast))
 
 
 
